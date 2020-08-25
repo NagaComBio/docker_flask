6c5be6f20dca34c190446bd59e8025821b63f866
Nagarajan <naga.rna@gmail.com>
2020-08-25 16:12:47 +0200

    a blog website protype

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+posts = [
+    {
+        'author': 'Nagarajan Paramasivam',
+        'title': 'Post 1',
+        'content': 'First post',
+        'date': 'Aug 24th 2020'
+    },
+    {
+        'author': 'John Oliver',
+        'title': 'Post 3',
+        'content': 'Third post',
+        'date': 'Aug 25th 2020'
+    }
+]
 
-@app.route("/family")
+@app.route("/")
+@app.route("/home")
+def hello_world():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
 def family():
-    return "Hello family!"
+    return render_template('about.html', title="About")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
